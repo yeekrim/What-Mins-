af72daa0de5c8fe1c694e6a88bce19e64834ea21
giljihun <accc45@outlook.com>
2023-06-01 13:31:02 +0900

    Initial Commit

---
+++ TimeSpeaker/TimeSpeakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeSpeakerApp.swift
+//  TimeSpeaker
+//
+//  Created by 길지훈 on 2023/06/01.
+//
+
+import SwiftUI
+
+@main
+struct TimeSpeakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
