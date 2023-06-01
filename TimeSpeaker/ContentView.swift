0c1c937accdc54e438d6a5be89894af6c5fc3de7
giljihun <accc45@outlook.com>
2023-06-01 17:03:18 +0900

    # 23.06.01.(Thursday) - 인터벌 설정 및 설정 시간마다 보이스알림 확인 - 상태 표시 추가

--- TimeSpeaker/ContentView.swift
+++ TimeSpeaker/ContentView.swift
@@ -8,15 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
+    //@State 속성은 값이 변경될 때마다 뷰를 새로 그리는 기능을 한다.
+    @State private var currentTime: String = ""
+    @State private var selectedInterval: Int = 1
+    @State private var isUpdatingTime = false
+    let speechSynthesizer = SpeechSynthesizer()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            Text("⚡️Activating⚡️")
+                .font(.subheadline)
+                .bold()
+                .italic()
+                .opacity(isUpdatingTime ? 1.0 : 0.0)
+            
+            Text(currentTime)
+                .font(.largeTitle)
+                .padding()
+
+            Text("Interval: \(selectedInterval) min" + (selectedInterval > 1 ? "s" : ""))
+                .padding()
+
+            HStack {
+                    Button(action: {
+                        if (selectedInterval > 1) {
+                            selectedInterval -= 1
+                        }
+                    }, label: {
+                        Image(systemName: "minus.circle")
+                    })
+                    
+                    Button(action: {
+                        startUpdatingTime()
+                    }, label: {
+                        Text("Start")
+                    })
+                
+                    Button(action: {
+                        stopUpdatingTime()
+                    }, label: {
+                        Text("Stop")
+                    })
+                
+                    Button(action: {
+                        selectedInterval += 1
+                    }, label: {
+                        Image(systemName: "plus.circle")
+                    })
+
+                }
+
+            
+
+            
         }
-        .padding()
+        .onAppear {
+            updateTime()
+        }
     }
+
+    func updateTime() {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        currentTime = formatter.string(from: Date())
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            updateTime()
+        }
+    }
+
+    func startUpdatingTime() {
+        stopUpdatingTime() // Stop previous updates if any
+        isUpdatingTime = true
+        updateTargetTime()
+    }
+
+    func stopUpdatingTime() {
+        isUpdatingTime = false
+    }
+
+    func updateTargetTime() {
+        let calendar = Calendar.current
+        let currentComponents = calendar.dateComponents([.hour, .minute, .second], from: Date())
+        guard let currentHour = currentComponents.hour,
+              let currentMinute = currentComponents.minute else {
+            return
+        }
+
+        let nextTargetMinute = (currentMinute + selectedInterval) % 60
+        let targetTime = calendar.date(bySettingHour: currentHour, minute: nextTargetMinute, second: 0, of: Date())!
+
+        let timeDiff = targetTime.timeIntervalSinceNow
+        DispatchQueue.main.asyncAfter(deadline: .now() + timeDiff) {
+            if self.isUpdatingTime {
+                self.speakCurrentTime()
+                self.updateTargetTime()
+            }
+        }
+    }
+
+    func speakCurrentTime() {
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            let timeString = formatter.string(from: Date())
+            speechSynthesizer.speak(timeString)
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ TimeSpeaker/SpeechSynthesizer.swift
@@ -0,0 +1,28 @@
+//
+//  SpeechSynthesizer.swift
+//  TimeSpeaker
+//
+//  Created by 길지훈 on 2023/06/01.
+//
+
+import AVFoundation
+
+class SpeechSynthesizer {
+    private let synthesizer: AVSpeechSynthesizer
+
+    init() {
+        self.synthesizer = AVSpeechSynthesizer()
+    }
+
+    func speak(_ text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-UK")
+        utterance.rate = 0.4
+
+        synthesizer.speak(utterance)
+    }
+
+    func stopSpeaking() {
+        synthesizer.stopSpeaking(at: .immediate)
+    }
+}
